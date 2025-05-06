d3b7ecc7f9374c46e10a00dee970253b2ccc91c4
Zachary Collier <zacharycollier36@protonmail.com>
2025-05-06 19:25:05 -0230

    Added code to Main.java including user input, calculations, and comparisons
    - Added functionality for user inputs for number of temperature entries and temperature entries themselves
    - Added calculations for temperature average
    - Added search functionality to find temperatures greater than temperature average

--- AvgTempCalc/src/main/java/org/example/Main.java
+++ AvgTempCalc/src/main/java/org/example/Main.java
@@ -1,9 +1,104 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
+// take user inputs (scanner)
+// judging by wording, ask for number of temperatures
+// calculate average temperature (sum of temperates / length of array)
+// determine which temps are greater than the average temperature
+
 public class Main {
     public static void main(String[] args) {
-        
+        int arrayLength;
+        double averageTemp;
+        double[] tempsArray;
+        int higherTempCount;
+        int higherTempPointer;
+        double[] isHigherTemp;
+        String exitChoice;
+
+        try (Scanner scanner = new Scanner(System.in)) {
+            do {
+            System.out.println("===Average Temperature Calculator===");
+            System.out.print("Enter number of daily temperature values to calculate: ");
+
+            // arrayLength input validation
+            while (true) {
+                if (!scanner.hasNextInt()) {
+                    System.out.println("Invalid input. Please enter a positive integer.");
+                    scanner.next();
+                } else {
+                    arrayLength = scanner.nextInt();
+                    if (arrayLength <= 0){
+                        System.out.println("Invalid input. Please enter a positive integer.");
+                        scanner.next();
+                    } else {
+                        break;
+                    }
+                }
+            }
+
+            // create array with length arrayLength
+            tempsArray = new double[arrayLength];
+            scanner.nextLine();
+
+            // Input each temperature
+            for (int i = 0; i < arrayLength; ++i) {
+                System.out.print(String.format("Enter temperate %d: ", i));
+
+                //input validation
+                while (true) {
+                    if (!scanner.hasNextDouble()) {
+                        System.out.println("Invalid input. Please enter a number.");
+                        scanner.next();
+                    } else {
+                        tempsArray[i] = scanner.nextDouble();
+                        scanner.nextLine();
+                        break;
+                    }
+                }
+            }
+
+            averageTemp = 0;
+            // calculate average temperature with sum of array values / array length
+            for (int i = 0; i < arrayLength; ++i){
+                averageTemp += tempsArray[i];
+            }
+            averageTemp /= arrayLength;
+
+            // determine which temps are above average
+            higherTempCount = 0;
+            for (int i = 0; i < arrayLength; ++i){
+                if (tempsArray[i] > averageTemp){
+                    ++higherTempCount;
+                }
+            }
+            isHigherTemp = new double[higherTempCount];
+            higherTempPointer = 0;
+            for (int i = 0; i < arrayLength; ++i){
+                if (tempsArray[i] > averageTemp){
+                    isHigherTemp[higherTempPointer] = tempsArray[i];
+                    ++higherTempPointer;
+                }
+            }
+
+            System.out.println("Temperatures: " + tempsArray.toString());
+            System.out.println("Average temperature: " + averageTemp);
+            System.out.println("Temperatures over average: " + isHigherTemp.toString());
+
+            System.out.println("Continue using this program? ('n' to exit, 'y' to continue)");
+                while (true) {
+                    exitChoice = scanner.nextLine();
+                    scanner.nextLine();
+                    if (exitChoice != "y" || exitChoice != "n") {
+                        System.out.println("Invalid input. Please enter either 'n' to exit or 'y' to continue.");
+                    } else {
+                        break;
+                    }
+                }
+            } while (exitChoice != "n");
+        } catch (Exception err) {
+            System.out.println("Error: " + err);
+        }
     }
 }
