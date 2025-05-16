dea8132d1990ab5c3f261a513a465a7fe497deba
Zachary Collier <zacharycollier36@protonmail.com>
2025-05-16 13:51:35 -0230

    Added Main and Seat classes

---
+++ movie-theatre-assignment/src/main/java/org/example/Main.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import java.util.Scanner;
+
+// Simulate a movie theater that helps users to book and reserve seats.
+// reserve seats (if seat is taken, let them know it is taken and suggest an available seat)
+// cancel seats
+// retrieve initial seating charting
+
+// 2d array for seat grid
+// each seat has a number-letter combo (string) (letter row, number column)
+// 65-90 ASCII are uppercase letters, 97-122 are lowercase
+// use regex to extract user input
+// search array for letter, then number
+// seats are either reserved or not reserved (boolean)
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
