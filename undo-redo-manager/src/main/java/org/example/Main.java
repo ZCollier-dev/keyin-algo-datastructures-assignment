144643aa5222a26e2398fcfb5261b2272f83d08b
Zachary Collier <zacharycollier36@protonmail.com>
2025-06-03 14:40:27 -0230

    Initialized undo/redo manager assignment

---
+++ undo-redo-manager/src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+/*
+ * Implememt an application that support undo/redo functionality. Use a linked list to maintain a sequence of states.\
+ * Each state change is stored as a node in the list, allowing for easy navigation
+ * 1<>2<>3<>4<>5
+
+Note: You should utilize generics to allow any datatype.
+* */
+
+public class Main {
+}
