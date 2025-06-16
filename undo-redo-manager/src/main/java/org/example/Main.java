bc6163911d7354f1f438997f486ecf25d503656b
Zachary Collier <zacharycollier36@protonmail.com>
2025-06-16 14:47:15 -0230

    Added test code to main.java
    Added testing code to main.java, testing all the methods in the UndoStates.java class

--- undo-redo-manager/src/main/java/org/example/Main.java
+++ undo-redo-manager/src/main/java/org/example/Main.java
@@ -1,13 +1,68 @@
 package org.example;
 
-/*
- * Implement an application that support undo/redo functionality.
- * Use a linked list to maintain a sequence of states.\
- * Each state change is stored as a node in the list, allowing for easy navigation
- * 1<>2<>3<>4<>5
-
-Note: You should utilize generics to allow any datatype.
-* */
-
 public class Main {
+    public static void main(String[] args) {
+        TestObject testObject = new TestObject(1, "Zack", true);
+        UndoStates<TestObject> undoStates = new UndoStates<>(5, TestObject::cloneObject);
+
+        undoStates.addToUndoList(testObject);
+        System.out.println(undoStates.retrieveItemAtCurrentPosition().toString());
+
+        testObject.setIsAvailable(false);
+        undoStates.addToUndoList(testObject);
+        System.out.println(undoStates.retrieveItemAtCurrentPosition().toString());
+
+        testObject.setName("Zachary");
+        undoStates.addToUndoList(testObject);
+        System.out.println(undoStates.retrieveItemAtCurrentPosition().toString());
+
+        testObject.setIsAvailable(true);
+        undoStates.addToUndoList(testObject);
+        System.out.println(undoStates.retrieveItemAtCurrentPosition().toString());
+
+        testObject.setIsAvailable(false);
+        testObject.setName("Za");
+        undoStates.addToUndoList(testObject);
+        System.out.println(undoStates.retrieveItemAtCurrentPosition().toString());
+
+        undoStates.moveBackThroughList();
+        System.out.println(undoStates.retrieveItemAtCurrentPosition().toString());
+        undoStates.moveBackThroughList();
+        System.out.println(undoStates.retrieveItemAtCurrentPosition().toString());
+
+        testObject.setName("Z");
+        undoStates.addToUndoList(testObject);
+        System.out.println(undoStates.retrieveItemAtCurrentPosition().toString());
+
+        undoStates.moveBackThroughList();
+        System.out.println(undoStates.retrieveItemAtCurrentPosition().toString());
+        undoStates.moveBackThroughList();
+        System.out.println(undoStates.retrieveItemAtCurrentPosition().toString());
+        undoStates.moveBackThroughList();
+        System.out.println(undoStates.retrieveItemAtCurrentPosition().toString());
+        undoStates.moveBackThroughList();
+        System.out.println(undoStates.retrieveItemAtCurrentPosition().toString());
+        undoStates.moveForwardThroughList();
+        System.out.println(undoStates.retrieveItemAtCurrentPosition().toString());
+        undoStates.moveForwardThroughList();
+        System.out.println(undoStates.retrieveItemAtCurrentPosition().toString());
+        undoStates.moveForwardThroughList();
+        System.out.println(undoStates.retrieveItemAtCurrentPosition().toString());
+        undoStates.moveForwardThroughList();
+        System.out.println(undoStates.retrieveItemAtCurrentPosition().toString());
+        undoStates.moveForwardThroughList();
+        System.out.println(undoStates.retrieveItemAtCurrentPosition().toString());
+
+        testObject.setName("Zac");
+        testObject.setIsAvailable(true);
+        undoStates.addToUndoList(testObject);
+        System.out.println(undoStates.retrieveItemAtCurrentPosition().toString());
+        testObject.setIsAvailable(false);
+        undoStates.addToUndoList(testObject);
+        System.out.println(undoStates.retrieveItemAtCurrentPosition().toString());
+
+        System.out.println();
+
+        System.out.println(undoStates);
+    }
 }
